f770fb45bf5af86c324adbd46a69b824e7bdb5ec
RadoslavaKatinina <147106851+RadoslavaKatinina@users.noreply.github.com>
2023-10-06 02:18:45 +0300

    Add files via upload

---
+++ SquareRoot.java
@@ -0,0 +1,7 @@
+package StructureOfProgram_03;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        System.out.print(Math.sqrt(8)+" "+Math.sqrt(353)+" "+Math.sqrt(78)+" "+Math.sqrt(3)+" "+Math.sqrt(-67)+" "+Math.sqrt(723)+" "+Math.sqrt(2000)+" "+Math.sqrt(-100));
+    }
+}
