04375992eefbb2d16fb94b712c2d035e6b92b350
RadoslavaKatinina <147106851+RadoslavaKatinina@users.noreply.github.com>
2023-10-06 01:59:58 +0300

    Add files via upload

---
+++ AbsoluteValue.java
@@ -0,0 +1,7 @@
+package StructureOfProgram_03;
+
+public class AbsoluteValue {
+    public static void main(String[] args) {
+        System.out.print(Math.abs(-147)+"\t"+Math.abs(15)+"\t"+Math.abs(0)+"\t"+Math.abs(18));
+    }
+}
