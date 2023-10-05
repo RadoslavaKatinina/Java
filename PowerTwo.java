983f7f3150e37fabefcbfad68654d23ed5cbcbdf
RadoslavaKatinina <147106851+RadoslavaKatinina@users.noreply.github.com>
2023-10-06 02:17:57 +0300

    Add files via upload

---
+++ PowerTwo.java
@@ -0,0 +1,7 @@
+package StructureOfProgram_03;
+
+public class PowerTwo {
+    public static void main(String[] args) {
+        System.out.print(Math.pow(-20,2)+" "+Math.pow(92,2)+" "+Math.pow(638,2)+" "+Math.pow(73,2)+" "+Math.pow(200,2)+" "+Math.pow(-22,2));
+    }
+}
