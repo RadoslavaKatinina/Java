e2dc471fc1d5d520b0f42b8002f43d6c85e00e17
RadoslavaKatinina <147106851+RadoslavaKatinina@users.noreply.github.com>
2023-10-06 02:13:19 +0300

    Add files via upload

---
+++ FirstName.java
@@ -0,0 +1,13 @@
+package StructureOfProgram_03;
+
+public class FirstName {
+    public static void main(String[] args) {
+        String firstName="Radoslava";
+        //name length
+        System.out.println(firstName.length());
+        //name uppercase
+        System.out.println(firstName.toUpperCase());
+        //name lowercase
+        System.out.println(firstName.toLowerCase());
+    }
+  }
