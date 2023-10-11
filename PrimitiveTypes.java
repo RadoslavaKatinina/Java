e296f1c4cd61cd1a5f8491b9fb7d44f0529770fc
Radoslava Katinina <147106851+RadoslavaKatinina@users.noreply.github.com>
2023-10-12 00:36:28 +0300

    Add files via upload

---
+++ PrimitiveTypes.java
@@ -0,0 +1,42 @@
+package Variables_04;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        short num1 = -250;
+        short num2 = 250;
+        int num3 = 4_589_498;
+        double num4 = 10_000_000_000_000_000_000d;
+        double num5 = -9_000_000_000_000_000_000.;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+
+        int num6 = 43_112;
+        int num7 = -1_357_674;
+        int num8 = 1_357_674;
+        int num9 = -1_357_674_000;
+        long num10 = 3_657_895_000L;
+        System.out.println(num6);
+        System.out.println(num7);
+        System.out.println(num8);
+        System.out.println(num9);
+        System.out.println(num10);
+
+        float num11 = 4.9876543f;
+        double num12 = 7.123456789012345678890;
+        double num13 = 18_398_458_438_583_853.28;
+        double num14 = 18_398_458_438_583_853.39875937284928422;
+        System.out.println(num11);
+        System.out.println(num12);
+        System.out.println(num13);
+        System.out.println(num14);
+
+
+
+
+
+    }
+}
