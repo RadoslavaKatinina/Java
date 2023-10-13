a2417d4868374d0c0e1cc773a522394fc3972154
Radoslava Katinina <147106851+RadoslavaKatinina@users.noreply.github.com>
2023-10-13 10:21:13 +0300

    Add files via upload

---
+++ Distance.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class Distance {
+    public static void main(String[] args) {
+        byte a = 5;
+        byte b = 8;
+        double distance = Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        System.out.print(distance);
+    }
+
+}
