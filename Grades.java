e883badbb49edd1f6c51f122e866d5f361e27eb4
Radoslava Katinina <147106851+RadoslavaKatinina@users.noreply.github.com>
2023-10-26 01:05:41 +0300

    Add files via upload

---
+++ Grades.java
@@ -0,0 +1,11 @@
+package InputOutput_06;
+
+public class Grades {
+    public static void main(String[] args) {
+        System.out.printf("| %-10s | %d |%n", "Иванчо", 2);
+        System.out.printf("| %-10s | %d |%n", "Ани", 5);
+        System.out.printf("| %-10s | %d |%n", "Марийка", 6);
+        System.out.printf("| %-10s | %d |%n", "Пенчо", 4);
+        System.out.printf("| %-10s | %d |", "Бориславчо", 5);
+    }
+}
